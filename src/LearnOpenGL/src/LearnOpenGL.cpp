65e274ee8d8ce2c3e92361f0a07931c8dac81689
Defu <gudo35@gmail.com>
2021-02-25 12:57:05 +0100

    Hello Window

--- src/LearnOpenGL/src/LearnOpenGL.cpp
+++ src/LearnOpenGL/src/LearnOpenGL.cpp
@@ -7,14 +7,42 @@ int main()
 {
     std::cout << "Hello world!\n";
 
+    // GLFG setup
     glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     auto window = glfwCreateWindow(640, 480, "Window", NULL, NULL);
+    if(window)
+    {
+        glfwMakeContextCurrent(window);
+        glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
+        {
+            glViewport(0, 0, width, height);
+        });  
 
-    const char* error;
-    glfwGetError(&error);
-    std::cout << error << "\n";
-    glfwShowWindow(window);
+        if(gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            glViewport(0, 0, 640, 480);
 
-    std::string input;
-    std::cin >> input;
+            while(!glfwWindowShouldClose(window))
+            {   
+                // Window Input
+                if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+                    glfwSetWindowShouldClose(window, true);
+
+                // Render
+                glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                // Swap buffers
+                glfwPollEvents();  
+                glfwSwapBuffers(window);  
+            }
+        }
+    }
+
+    // Close
+    glfwTerminate();
 }
