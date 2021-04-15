50bee298b7b760b46cc1b96b32472e5743ff3da5
Defu <gudo35@gmail.com>
2021-04-15 18:32:21 +0200

    Textures - Exercise 1

--- resources/shaders/fragment.glsl
+++ resources/shaders/fragment.glsl
@@ -9,5 +9,6 @@ uniform sampler2D ourTexture2;
 
 void main()
 {
-    fragColor = mix(texture(ourTexture1, textureCoords), texture(ourTexture2, textureCoords), 0.3);
+    vec2 faceCoords = vec2(-textureCoords.x, textureCoords.y);
+    fragColor = mix(texture(ourTexture1, textureCoords), texture(ourTexture2, faceCoords), 0.3);
 }
