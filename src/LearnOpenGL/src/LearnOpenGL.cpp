402a4a97dbe1112232e68709ecb48b72c37efc1d
Defu <gudo35@gmail.com>
2021-04-17 13:52:22 +0200

    Transformations - Exercise 1

--- src/LearnOpenGL/src/LearnOpenGL.cpp
+++ src/LearnOpenGL/src/LearnOpenGL.cpp
@@ -130,8 +130,8 @@ int main()
 
                 // Transform
                 glm::mat4 transform{1.0f};
-                transform = glm::translate(transform, glm::vec3{0.5f, -0.5f, 0.f});
                 transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3{0, 0, 1.f});
+                transform = glm::translate(transform, glm::vec3{0.5f, -0.5f, 0.f});
 
                 unsigned int transformLoc = glGetUniformLocation(shaderProg.ID, "transform");
                 glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
