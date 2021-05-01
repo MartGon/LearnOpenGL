07a1f583b86c7df1e0e3ca8393323910304f42e0
Defu <gudo35@gmail.com>
2021-05-01 16:48:51 +0200

    Assimp

--- src/LearnOpenGL/src/LearnOpenGL.cpp
+++ src/LearnOpenGL/src/LearnOpenGL.cpp
@@ -8,6 +8,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
 #include <iostream>
 #include <vector>
 #include <cmath>
