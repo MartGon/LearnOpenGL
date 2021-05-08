c2a6a43a5c76be6ea6c6bbb3db0e234c311ed32f
Defu <gudo35@gmail.com>
2021-05-08 11:52:43 +0200

    Framebuffers Pt. 2.2.2 - Weighted GrayScale

--- resources/shaders/quadFrag.glsl
+++ resources/shaders/quadFrag.glsl
@@ -9,6 +9,6 @@ uniform sampler2D screenTexture;
 void main()
 {
     FragColor = texture(screenTexture, TexCoords);
-    float average = (FragColor.r + FragColor.g + FragColor.b) / 3.0;
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g  + 0.0722 * FragColor.b;
     FragColor = vec4(vec3(average), 1.0);
 }
