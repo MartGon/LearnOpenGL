0185a539e99713c8e785ec6dd8d8bb1133a3a732
Defu <gudo35@gmail.com>
2021-05-08 12:06:56 +0200

    Framebuffers Pt. 2.3 - Kernel Effects

--- resources/shaders/quadFrag.glsl
+++ resources/shaders/quadFrag.glsl
@@ -6,9 +6,35 @@ in vec2 TexCoords;
 
 uniform sampler2D screenTexture;
 
+const float offset = 1.0 / 300.0;  
+
 void main()
 {
-    FragColor = texture(screenTexture, TexCoords);
-    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g  + 0.0722 * FragColor.b;
-    FragColor = vec4(vec3(average), 1.0);
+    vec2 offsets[9] = vec2[](
+        vec2(-offset,  offset), // top-left
+        vec2( 0.0f,    offset), // top-center
+        vec2( offset,  offset), // top-right
+        vec2(-offset,  0.0f),   // center-left
+        vec2( 0.0f,    0.0f),   // center-center
+        vec2( offset,  0.0f),   // center-right
+        vec2(-offset, -offset), // bottom-left
+        vec2( 0.0f,   -offset), // bottom-center
+        vec2( offset, -offset)  // bottom-right    
+    );
+
+    float kernel[9] = float[](
+        -1, -1, -1,
+        -1,  9, -1,
+        -1, -1, -1
+    );
+
+    vec3 pixels[9];
+    for(int i = 0; i < 9; i++)
+        pixels[i] = vec3(texture(screenTexture, TexCoords + offsets[i]));
+
+    vec3 color = vec3(0.0);
+    for(int i = 0; i < 9; i++)
+        color += pixels[i] * kernel[i];
+
+    FragColor = vec4(color, 1.0);
 }
